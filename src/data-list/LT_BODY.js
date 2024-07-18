bf3acbd68fce931fdba39ad62e1e4dcffc6070e4
Angel Gala <galafloresangelemilio@gmail.com>
2024-07-18 15:46:50 -0500

    refactored landing

---
+++ src/data-list/LT_BODY.js
@@ -0,0 +1,128 @@
+export const LT_BODY = {
+  PRINCIPAL_SECTION: {
+    type: "banner",
+    banner: {
+      bannerImgUrl: "/images/banner.png",
+      bannerImgAlt: "Proyectores Servicio Proyectores",
+    },
+    carousel: {
+      images: [
+        {
+          imageUrl: "",
+          imageAlt: "Image1",
+        },
+      ],
+    },
+  },
+  ABOUT_US: {
+    title: "sobre nosotros",
+    description:
+      "Nos dedicamos a transformar la manera en que experimentas el entretenimiento y las presentaciones visuales. Desde nuestros comienzos, hemos tenido una misión clara: proporcionar soluciones de proyección de alta calidad que se adapten a las necesidades de nuestros clientes, ya sea en el hogar, la oficina, o cualquier otro entorno.",
+    imgUrl: "/images/about-us-banner.webp",
+    imgAlt: "Sobre nosotros",
+  },
+  SEO_SECTION: {
+    title: "Venta de lámparas de alta calidad",
+    description:
+      "Ofrecemos lámparas de proyectores de alta calidad y larga duración. Compatibles con varias marcas, con precios competitivos y envío rápido. Mejore su proyección con nuestro soporte especializado.",
+    image: {
+      imageUrl: "/images/about-us-banner.webp",
+      imageAlt: "Image 1",
+    },
+  },
+  SERVICES: {
+    title: "Algunos de nuestros servicios",
+    list: [
+      {
+        imgUrl: "./images/imagen-borrosa.webp",
+        name: "",
+      },
+      {
+        imgUrl: "./images/imagen-con-puntos.webp",
+        name: "",
+      },
+      {
+        imgUrl: "./images/imagen-distorsionada.webp",
+        name: "",
+      },
+      {
+        imgUrl: "./images/lampara-no-funciona.webp",
+        name: "",
+      },
+      {
+        imgUrl: "./images/mensaje-sin-senal.webp",
+        name: "",
+      },
+      {
+        imgUrl: "./images/proyector-sobrecalienta.webp",
+        name: "",
+      },
+      {
+        imgUrl: "./images/se-apaga-y-reinicia.webp",
+        name: "",
+      },
+    ],
+  },
+  PRODUCTS: {
+    title: "",
+    list: [
+      {
+        imgUrl: "/images/se-apaga-y-reinicia.webp",
+        name: "Image 1",
+      },
+      {
+        imgUrl: "/images/se-apaga-y-reinicia.webp",
+        name: "Image 2",
+      },
+      {
+        imgUrl: "/images/se-apaga-y-reinicia.webp",
+        name: "Image 3",
+      },
+    ],
+  },
+
+  BRANDS:{
+    list:[
+      {
+        name: "BenQ",
+        imageUrl: "./images/logo-benq.webp",
+      },
+      {
+        name: "Epson",
+        imageUrl: "./images/logo-epson.webp",
+      },
+      {
+        name: "inFocus",
+        imageUrl: "./images/logo-infocus.webp",
+      },
+      {
+        name: "LG",
+        imageUrl: "./images/logo-lg.webp",
+      },
+      {
+        name: "Optoma",
+        imageUrl: "./images/logo-optoma.webp",
+      },
+      {
+        name: "Sony",
+        imageUrl: "./images/logo-sony.webp",
+      },
+      {
+        name: "Hitachi",
+        imageUrl: "./images/logo-hitachi.webp",
+      },
+      {
+        name: "ViewSonic",
+        imageUrl: "./images/logo-viewsonic.webp",
+      },
+      {
+        name: "Acer",
+        imageUrl: "./images/logo-acer.webp",
+      },
+      {
+        name: "Panasonic",
+        imageUrl: "./images/logo-panasonic.webp",
+      },
+    ],
+  },
+};
---
+++ src/data-list/LT_SETTINGS.js
@@ -0,0 +1,32 @@
+import { LT_HEADER } from "./LT_HEADER.js";
+import { LT_FOOTER } from "./LT_FOOTER.js";
+import { LT_BODY } from "./LT_BODY.js";
+
+export const LT_SETTINGS = {
+  PROJECT: {
+    name: "Servicio Proyectores",
+    logo: "./images/logo.webp",
+    favicon: "./images/logo.webp",
+    slogan:
+      "Estamos aquí para ayudarte a encontrar el proyector perfecto para tus necesidades. No dudes en ponerte en contacto con nosotros para obtener más información sobre nuestros productos y servicios.",
+    keywords: "reparacion, mantenimiento, proyectores, lamparas",
+  },
+  UI_DESIGN: {
+    COLORS: {
+      primary: "",
+      secondary: "",
+
+      bg: "",
+      color: "",
+    },
+  },
+  TOP_HEADER: {},
+  HEADER: LT_HEADER,
+  BODY: LT_BODY,
+  FOOTER: LT_FOOTER,
+  CONTACT_DATA: {
+    numbers: ["931 912 165"],
+    wsp: ["931912165"],
+  },
+  CREATE_BY: "Agencia Servitec",
+};
