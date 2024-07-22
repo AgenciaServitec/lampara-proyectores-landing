3c2ff9cddb83fd34ba729e896674db2222969d20
Angel Gala <galafloresangelemilio@gmail.com>
2024-07-22 12:22:54 -0500

    Updated images of services and products

--- src/data-list/LT_BODY.js
+++ src/data-list/LT_BODY.js
@@ -18,7 +18,7 @@ export const LT_BODY = {
     title: "sobre nosotros",
     description:
       "Nos dedicamos a transformar la manera en que experimentas el entretenimiento y las presentaciones visuales. Desde nuestros comienzos, hemos tenido una misión clara: proporcionar soluciones de proyección de alta calidad que se adapten a las necesidades de nuestros clientes, ya sea en el hogar, la oficina, o cualquier otro entorno.",
-    imgUrl: "/images/about-us-banner.webp",
+    imgUrl: "/images/about-us.webp",
     imgAlt: "Sobre nosotros",
   },
   SEO_SECTION: {
@@ -26,7 +26,7 @@ export const LT_BODY = {
     description:
       "Ofrecemos lámparas de proyectores de alta calidad y larga duración. Compatibles con varias marcas, con precios competitivos y envío rápido. Mejore su proyección con nuestro soporte especializado.",
     image: {
-      imageUrl: "/images/about-us-banner.webp",
+      imageUrl: "/images/about-us.webp",
       imageAlt: "Image 1",
     },
   },
@@ -34,55 +34,50 @@ export const LT_BODY = {
     title: "Algunos de nuestros servicios",
     list: [
       {
-        imgUrl: "./images/imagen-borrosa.webp",
+        imgUrl: "/images/services/imagen-borrosa.webp",
         name: "",
       },
       {
-        imgUrl: "./images/imagen-con-puntos.webp",
+        imgUrl: "/images/services/imagen-con-puntos.webp",
         name: "",
       },
       {
-        imgUrl: "./images/imagen-distorsionada.webp",
+        imgUrl: "/images/services/imagen-distorsionada.webp",
         name: "",
       },
       {
-        imgUrl: "./images/lampara-no-funciona.webp",
+        imgUrl: "/images/services/mensaje-sin-senial.webp",
         name: "",
       },
       {
-        imgUrl: "./images/mensaje-sin-senal.webp",
+        imgUrl: "/images/services/proyector-se-sobrecalienta.webp",
         name: "",
       },
       {
-        imgUrl: "./images/proyector-sobrecalienta.webp",
-        name: "",
-      },
-      {
-        imgUrl: "./images/se-apaga-y-reinicia.webp",
+        imgUrl: "./images/services/apaga-y-reinicia.webp",
         name: "",
       },
     ],
   },
   PRODUCTS: {
-    title: "",
+    title: "Lámparas",
     list: [
       {
-        imgUrl: "/images/se-apaga-y-reinicia.webp",
-        name: "Image 1",
+        imgUrl: "/images/products/bl-fp280c-optoma.webp",
+        name: "BL-FP280c para OPTOMA",
       },
       {
-        imgUrl: "/images/se-apaga-y-reinicia.webp",
-        name: "Image 2",
+        imgUrl: "/images/products/elplp71-epson.webp",
+        name: "LMP-D213 para SONY",
       },
       {
-        imgUrl: "/images/se-apaga-y-reinicia.webp",
-        name: "Image 3",
+        imgUrl: "/images/products/lmp-d213-sony.webp",
+        name: "ELPLP71 para EPSON",
       },
     ],
   },
-
-  BRANDS:{
-    list:[
+  BRANDS: {
+    list: [
       {
         name: "BenQ",
         imageUrl: "./images/logo-benq.webp",
