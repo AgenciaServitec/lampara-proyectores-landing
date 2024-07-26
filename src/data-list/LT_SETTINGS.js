32ee4c0c5255991d0e25f6eb84fa2c92f7430161
Angel Gala <galafloresangelemilio@gmail.com>
2024-07-26 18:04:32 -0500

    added google ads tag and keywords

--- src/data-list/LT_SETTINGS.js
+++ src/data-list/LT_SETTINGS.js
@@ -7,9 +7,8 @@ export const LT_SETTINGS = {
     name: "Lámparas Proyectores",
     logo: "./images/logo.webp",
     favicon: "./images/logo.webp",
-    slogan:
-      "Estamos aquí para ayudarte a encontrar el proyector perfecto para tus necesidades. No dudes en ponerte en contacto con nosotros para obtener más información sobre nuestros productos y servicios.",
-    keywords: "reparacion, mantenimiento, proyectores, lamparas",
+    slogan: "Estamos aquí para ayudarte a encontrar el proyector perfecto para tus necesidades. No dudes en ponerte en contacto con nosotros para obtener más información sobre nuestros productos y servicios.",
+    keywords: "proyector epson, proyectores epson, epson proyector, lampara proyector, proyector optoma, proyector epson precio, proyector sony, proyectores precios, venta de proyectores, soporte para proyector, soporte proyector, epson elplp71, benq proyector, precio de proyector epson, lampara proyector optoma, precios de proyectores, viewsonic proyector, lampara proyector epson, proyector de imagen, video proyector epson, lampara para proyector, lampara de proyector, proyector panasonic, sony proyector, Lamparas Proyectores, Lampara para Proyector",
   },
   UI_DESIGN: {
     COLORS: {
